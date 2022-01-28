e9c5b65aa79fcfc4b6b2d8e24ee5228bb6d9bba0
Arkadiusz Wieczorek <kontakt@arkadiusz-wieczorek.pl>
2022-01-28 11:33:44 +0100

    Update esbuild.config and manifest

--- esbuild.config.js
+++ esbuild.config.js
@@ -2,6 +2,13 @@ import esbuild from 'esbuild';
 import scss from 'esbuild-plugin-sass';
 import svg from 'esbuild-plugin-svgr';
 
+const watch = process.argv.includes('--watch') && {
+    onRebuild(error) {
+        if (error) console.error('[watch] build failed', error);
+        else console.log('[watch] build finished');
+    },
+};
+
 esbuild
     .build({
         entryPoints: [
@@ -11,17 +18,11 @@ esbuild
             'background.ts',
         ],
         bundle: true,
+        minify: true,
         outdir: './lib',
         loader: { '.woff': 'file', '.woff2': 'file' },
         plugins: [scss(), svg()],
-        watch: {
-            onRebuild(error, result) {
-                try {
-                    console.log('watch build succeeded:', result);
-                } catch (error) {
-                    console.error('watch build failed:', error);
-                }
-            },
-        },
+        watch,
     })
+    .then(() => console.log('Add-on was built'))
     .catch(() => process.exit(1));
